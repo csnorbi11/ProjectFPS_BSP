a285644917a9ff550e5af7761a864498ff50c12e
csnorbi11 <csaszar.norbi@gmail.com>
2026-01-06 14:08:45 +0100

    update README and screenshot

--- src/graphics/ParticleEmitter.hpp
+++ src/graphics/ParticleEmitter.hpp
@@ -1,10 +1,16 @@
 #ifndef PARTICLEEMITTER_HPP
 #define PARTICLEEMITTER_HPP
 
+#include "Particle.hpp"
+#include <vector>
+
 class ParticleEmitter {
 public:
 	ParticleEmitter() = default;
 	~ParticleEmitter() = default;
+
+private:
+	std::vector<Particle> particles{};
 };
 
 
